9f6e7c68efb8dc228a8720ae6b651daad73e6125
Federico Catalini <s1087895@studenti.univpm.it>
2020-10-06 13:07:26 +0200

    Struttura iniziale esempio

--- app/src/main/java/ProgettoOOP/app/controller/restController.java
+++ app/src/main/java/ProgettoOOP/app/controller/restController.java
@@ -1,8 +1,21 @@
 package ProgettoOOP.app.controller;
 
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+
+import ProgettoOOP.app.model.getCovidCases;
 
 @RestController
 public class restController {
-
+	@GetMapping("/hello")
+	public getCovidCases exampleMethod(@RequestParam(name="param1", defaultValue="World") String param1) {
+		return new getCovidCases("Mario", "Rossi");
+	}
+	@PostMapping("/hello")
+	public getCovidCases exampleMethod2(@RequestBody getCovidCases body) {
+		return body;
+	}
 }
---
+++ app/src/main/java/ProgettoOOP/app/model/getCovidCases.java
@@ -0,0 +1,23 @@
+package ProgettoOOP.app.model;
+
+public class getCovidCases {
+	private String name;
+	private String surname;
+	public getCovidCases(String name, String surname) {
+		this.name = name;
+		this.surname = surname;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public String getSurname() {
+		return surname;
+	}
+	public void setSurname(String surname) {
+		this.surname = surname;
+	}
+
+}
