7b906d8eaec399a2c1f8197be184d1f47572713a
Federico Catalini <s1087895@studenti.univpm.it>
2020-10-06 10:05:30 +0200

    Struttura iniziale

---
+++ app/src/test/java/ProgettoOOP/app/AppApplicationTests.java
@@ -0,0 +1,13 @@
+package ProgettoOOP.app;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
