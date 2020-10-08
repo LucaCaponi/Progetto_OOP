324336a523c1f0c747f86fde37b02fa4fc3c503b
Federico Catalini <s1087895@studenti.univpm.it>
2020-10-08 12:11:17 +0200

    Modifica Model

--- app/src/main/java/ProgettoOOP/app/controller/restController.java
+++ app/src/main/java/ProgettoOOP/app/controller/restController.java
@@ -7,14 +7,23 @@ import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
-import ProgettoOOP.app.model.getCountryContinent;
+import ProgettoOOP.app.model.getCountryAllStatus;
 
 @RestController
 public class restController {
+	
 	@RequestMapping(value = "/premium/country/{{country}}", method = RequestMethod.GET)
 	public ResponseEntity<Object> getCountryAllStatus(@RequestParam(name="from", defaultValue="2020-03-01T00:00:00Z") String from,
 			@RequestParam(name="to", defaultValue="2020-04-01T00:00:00Z") String to) 
 	{	
 		return new ResponseEntity<>();
 	}
+	
+	@RequestMapping(value = "/premium/summary", method = RequestMethod.GET)
+	public ResponseEntity<Object> getSummary(@RequestParam(name="from", defaultValue="2020-03-01T00:00:00Z") String from,
+			@RequestParam(name="to", defaultValue="2020-04-01T00:00:00Z") String to) 
+	{	
+		return new ResponseEntity<>();
+	}
+	
 }
