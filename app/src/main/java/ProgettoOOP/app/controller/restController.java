3888adfe2c54d0a252e48ce306667d113bbe3819
Federico Catalini <s1087895@studenti.univpm.it>
2020-10-11 19:10:02 +0200

    RestController modifica

--- app/src/main/java/ProgettoOOP/app/controller/restController.java
+++ app/src/main/java/ProgettoOOP/app/controller/restController.java
@@ -7,6 +7,7 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import ProgettoOOP.app.model.Countries;
@@ -45,22 +46,13 @@ public class restController {
 		return new ResponseEntity<>("Country is created successfully", HttpStatus.CREATED);
 	}
 	
-	/* 
-	 * 
-	 * @RequestMapping(value = "/premium/country/{{country}}", method = RequestMethod.GET)
+	/*
+	@RequestMapping(value = "/country/{{country}}", method = RequestMethod.GET)
 	public ResponseEntity<Object> getCountryAllStatus(@RequestParam(name="from", defaultValue="2020-03-01T00:00:00Z") String from,
 			@RequestParam(name="to", defaultValue="2020-04-01T00:00:00Z") String to) 
 	{	
 		return new ResponseEntity<>();
 	}
-	
-	@RequestMapping(value = "/premium/summary", method = RequestMethod.GET)
-	public ResponseEntity<Object> getSummary(@RequestParam(name="from", defaultValue="2020-03-01T00:00:00Z") String from,
-			@RequestParam(name="to", defaultValue="2020-04-01T00:00:00Z") String to) 
-	{	
-		return new ResponseEntity<>();
-	}
-	
 	*/
 	
 }
